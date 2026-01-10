cb5dd69a8fb748f4d415443f5641d77742717013
space <55336451+spacebytee@users.noreply.github.com>
2026-01-10 14:32:27 -0500

    prevent chat clear on disconnect

---
+++ src/main/java/com/bytespacegames/chattingenthusiast/mixin/GuiMixin.java
@@ -0,0 +1,22 @@
+package com.bytespacegames.chattingenthusiast.mixin;
+
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.components.ChatComponent;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Gui.class)
+public class GuiMixin {
+    @Redirect(
+            method = "onDisconnected",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/components/ChatComponent;clearMessages(Z)V",
+                    ordinal=0
+            )
+    )
+    public void mixin$onDisconnected(ChatComponent instance, boolean bl) {
+
+    }
+}
