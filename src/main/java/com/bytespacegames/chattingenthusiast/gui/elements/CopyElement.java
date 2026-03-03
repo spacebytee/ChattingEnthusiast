b4aaa08528c18b7ec20d12bcc269dbfa1202aea9
space <55336451+spacebytee@users.noreply.github.com>
2026-03-03 17:37:00 -0500

    fadeless fix + ctrl

--- src/main/java/com/bytespacegames/chattingenthusiast/gui/elements/CopyElement.java
+++ src/main/java/com/bytespacegames/chattingenthusiast/gui/elements/CopyElement.java
@@ -60,7 +60,7 @@ public class CopyElement extends AbstractGuiElement {
             List<Component> tooltip = new ArrayList<>();
             tooltip.add(Component.translatable("chattingenthusiast.tooltip.title"));
             tooltip.add(Component.translatable("chattingenthusiast.tooltip.copy"));
-            tooltip.add(Component.translatable("chattingenthusiast.tooltip.alt"));
+            tooltip.add(Component.translatable("chattingenthusiast.tooltip.ctrl"));
             if (FabricLoader.getInstance().isModLoaded("chatshot")) tooltip.add(Component.translatable("chattingenthusiast.tooltip.shift"));
             graphics.setTooltipForNextFrame(Minecraft.getInstance().font,tooltip,Optional.empty(),GuiManager.getMouseX(),GuiManager.getMouseY());
         }
